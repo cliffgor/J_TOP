a5f8712f157637cf784c41d5205b7f88c506f893
Cliff Gor <clierozigy@gmail.com>
2019-04-14 23:21:20 +0300

    Javascript Objects: 2. Factory functions

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-// CREATING OBJECTS
-// object literal IS A SIMPLE WAY TO DEFINE AN OBJECT
-const circle ={
-    radius: 1,
-    location: {
-        x:1,
-        y:1
-    },
+// FACTORIE Function
+
+
+function createCircle(radius){
+    return{
+    radius,
     draw: function(){
         console.log('draw');
-    }  
-}
-circle.draw();
+    }
+    
+    };
+} 
+const  circle = createCircle(1); 
 
-// the circle has three properties and draw is a method
-// properties used to hold values
-
-const circle ={
-    radius: 1,
-    location: {
-        x:1,
-        y:1
-    },
-    draw: function(){
-        console.log('draw');
-    }  
-}
 circle.draw();
