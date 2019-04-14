071448531f6201fa910674f8e2441f9834387554
Cliff Gor <clierozigy@gmail.com>
2019-04-14 23:10:02 +0300

    Javascript Objects: 1. Created objects and assigned the object with poperties and a method

---
+++ index.js
@@ -0,0 +1,28 @@
+// CREATING OBJECTS
+// object literal IS A SIMPLE WAY TO DEFINE AN OBJECT
+const circle ={
+    radius: 1,
+    location: {
+        x:1,
+        y:1
+    },
+    draw: function(){
+        console.log('draw');
+    }  
+}
+circle.draw();
+
+// the circle has three properties and draw is a method
+// properties used to hold values
+
+const circle ={
+    radius: 1,
+    location: {
+        x:1,
+        y:1
+    },
+    draw: function(){
+        console.log('draw');
+    }  
+}
+circle.draw();
